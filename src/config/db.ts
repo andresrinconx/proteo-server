add4bd10a7e77af89f3a2d928ed123dad6dcd4db
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-11-07 17:15:51 -0400

    semi

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,10 @@
-import { createPool } from 'mysql2'
+import { createPool } from 'mysql2';
 
 const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
-})
+});
 
-export default pool
+export default pool;
