e7aab0bf1fd1af153af0f05c894758354c00de0f
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-11-08 17:25:02 -0400

    changes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -42,31 +42,30 @@ export const auth = async (req: Request, res: Response) => {
  * Create user permission, check supervisors and send push notification.
  */
 export const createPermission = async (req: Request, res: Response) => {
-  const { token } = req.body;
   // const { user, lugar, codigo, tiposol, tipomot, fechainicial, fechafinal, horasalida, horaentrada, totalhoras, motivo, horacita } = req.body
 
   // create permission
-  // const permission = await query(``);
-  // console.log(permission);
+  const permission = await query(``);
+  console.log(permission);
 
-  // // get supervisors tokens
-  // const supervisors = await query('');
+  // get supervisors tokens
+  const supervisors = await query('');
 
-  // // not found
-  // if ((supervisors as [])?.length < 1) {
-  //   const error = new Error('Supervisors not found');
-  //   return res.status(404).json({ msg: error.message });
-  // }
+  // not found
+  if ((supervisors as [])?.length < 1) {
+    const error = new Error('Supervisors not found');
+    return res.status(404).json({ msg: error.message });
+  }
 
-  // // send push notification
-  // const tokens = [];
+  // send push notification
+  const tokens = [];
   try {
-    await getMessaging().send({
+    await getMessaging().sendEachForMulticast({
       notification: {
         title: 'Test',
         body: 'Test Notification',
       },
-      token
+      tokens
     });
     res.status(200).json({ msg: 'Messages sent successfully' });
   } catch (error) {
