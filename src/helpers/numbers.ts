ebdfabd437f9ededac5c753c3b2d216a42c11bcb
doniel <donielacosta1995@gmail.com>
2023-11-09 10:51:33 -0400

    helpert autoincremet zerializate in ts

--- src/helpers/numbers.ts
+++ src/helpers/numbers.ts
@@ -2,9 +2,7 @@
 export const  strPad = (str, pad_length, pad_string, pad_type)=>{
 
 	const len = pad_length - str.length;
-
 	if(len < 0) return str;
-
 	const pad = new Array(len + 1).join(pad_string);
     
 	if(pad_type == 'STR_PAD_LEFT') return pad + str;
