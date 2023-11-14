4b2b5488b4f28f1f73c0d183c33fdf296a6153ed
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-11-14 09:57:20 -0400

    cache implemented

--- src/helpers/currencies.ts
+++ src/helpers/currencies.ts
@@ -6,11 +6,23 @@ const agent = new https.Agent({
   rejectUnauthorized: false
 });
 
+
 /**
  * Get BCV dollar price
- */
+*/
+const cache = {
+  dollarPriceTime: 0, // moment setted
+  dollarPrice: '',
+  expirationTime: 43200000, // 12 hours
+};
+
 export const bcvDollarPrice = async () => {
   try {
+    // check cache
+    if (cache.dollarPrice && Date.now() - cache.expirationTime < cache.dollarPriceTime) {
+      return cache.dollarPrice;
+    }
+
     // get page
     const res = await axios.get('https://www.bcv.org.ve/', { httpsAgent: agent });
     const page = await res.data;
@@ -19,7 +31,11 @@ export const bcvDollarPrice = async () => {
     const $ = cheerio.load(page);
     const dollarPrice = $('#dolar .field-content .row .centrado strong').text();
 
-    return dollarPrice;
+    // set cache
+    cache.dollarPrice = dollarPrice.trim();
+    cache.dollarPriceTime = Date.now();
+
+    return dollarPrice.trim();
   } catch (error) {
     console.log(error);
   }
